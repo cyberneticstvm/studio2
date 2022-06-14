61b4e72f8bec11b841a764825b680d0504c21da9
CYBER NETICS <cybernetics.me@outlook.com>
2022-06-14 08:50:37 +0530

    logo updated

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -58,7 +58,92 @@
 	<body data-plugin-scroll-spy data-plugin-options="{'target': '#header'}">
 
 		<div class="body">
+			<div class="sticky-wrapper sticky-wrapper-transparent sticky-wrapper-effect-1 sticky-wrapper-effect-1-dark sticky-wrapper-border-bottom" data-plugin-sticky data-plugin-options="{'minWidth': 0, 'stickyStartEffectAt': 100, 'padding': {'top': 0}}">
+				<div class="sticky-body">
+					<div class="container container-xl-custom">
+						<div class="row justify-content-between align-items-center">
+							<div class="col-auto">
+								<div class="py-4">
+									<a href="demo-architecture-interior.html">
+										<img alt="Porto" width="153" height="53" src="img/demos/architecture-interior/studio1-creators-logo.png">
+									</a>
+								</div>
+							</div>
+							<div class="col-auto text-end d-flex align-items-center justify-content-end">
+								<a href="tel:+1234567890" class="text-color-light font-weight-bold text-decoration-none d-none d-sm-block me-2">CALL US NOW! <span class="font-weight-light">1-800-123-4567</span></a>
+								<ul class="social-icons social-icons-clean social-icons-icon-light d-none d-md-inline-block mx-4">
+									<li class="social-icons-facebook"><a href="http://www.facebook.com/" target="_blank" title="Facebook"><i class="fab fa-facebook-f"></i></a></li>
+									<li class="social-icons-twitter"><a href="http://www.twitter.com/" target="_blank" title="Twitter"><i class="fab fa-twitter"></i></a></li>
+									<li class="social-icons-linkedin"><a href="http://www.linkedin.com/" target="_blank" title="Linkedin"><i class="fab fa-linkedin-in"></i></a></li>
+								</ul>
+								<button class="hamburguer-btn hamburguer-btn-light" data-set-active="false">
+									<span class="hamburguer">
+										<span></span>
+										<span></span>
+										<span></span>
+									</span>
+								</button>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<header id="header" class="side-header-overlay-full-screen side-header-hide" data-plugin-options="{'stickyEnabled': false}">
+
+				<button class="hamburguer-btn hamburguer-btn-light hamburguer-btn-side-header hamburguer-btn-side-header-overlay active" data-set-active="false">
+					<span class="close">
+						<span></span>
+						<span></span>
+					</span>
+				</button>
+
+				<div class="header-body d-flex h-100">
+					<div class="header-column flex-row flex-lg-column justify-content-center h-100">
+						<div class="header-container container d-flex h-100">
+							<div class="header-row header-row-side-header flex-row h-100 pb-5">
+								<div class="side-header-scrollable scrollable colored-slider h-50" data-plugin-scrollable>
+									<div class="scrollable-content">
+										<div class="header-nav header-nav-light-text header-nav-links header-nav-links-side-header header-nav-links-vertical header-nav-links-vertical-expand header-nav-click-to-open align-self-start">
+											<div class="header-nav-main header-nav-main-square header-nav-main-dropdown-no-borders header-nav-main-effect-4 header-nav-main-sub-effect-1">
+												<nav>
+													<ul class="nav nav-pills" id="mainNav">
+														<li class="dropdown">
+															<a class="dropdown-item active" data-hash data-hash-offset="0" data-hash-offset-lg="95" href="#home">Home</a>
+														</li>
+														<li class="dropdown">
+															<a class="dropdown-item" data-hash data-hash-offset="0" data-hash-offset-lg="95" href="#whoweare">Who We Are</a>
+														</li>
+														<li class="dropdown">
+															<a class="dropdown-item" data-hash data-hash-offset="0" data-hash-offset-lg="95" href="#ourservices">Our Services</a>
+														</li>
+														<li class="dropdown">
+															<a class="dropdown-item" data-hash data-hash-offset="0" data-hash-offset-lg="95" href="#ourprojects">Our Projects</a>
+														</li>
+														<li class="dropdown">
+															<a class="dropdown-item" data-hash data-hash-offset="0" data-hash-offset-lg="95" href="#whyhireus">Why Hire Us</a>
+														</li>
+														<li class="dropdown">
+															<a class="dropdown-item" data-hash data-hash-offset="0" data-hash-offset-lg="95" href="#ourteam">Our Team</a>
+														</li>
+														<li class="dropdown">
+															<a class="dropdown-item" data-hash data-hash-offset="0" data-hash-offset-lg="95" href="#contactus">Contact Us</a>
+														</li>
+													</ul>
+												</nav>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</header>
+
+			<div role="main" class="main bg-dark">
             @yield("content")
+			</div>
         </div>
     <!-- Vendor -->
 		<script src="vendor/jquery/jquery.min.js"></script>
